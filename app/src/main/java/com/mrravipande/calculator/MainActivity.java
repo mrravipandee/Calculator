0a3cb09e24f365cd38defd69f95265e9ecf82a99
Ravi Pandey <imravipanday@gmail.com>
2023-09-02 21:32:27 +0530

    have a error, theme, logical big problems cant show throw an error message.

--- app/src/main/java/com/mrravipande/calculator/MainActivity.java
+++ app/src/main/java/com/mrravipande/calculator/MainActivity.java
@@ -6,43 +6,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView result;
-    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, bZero, bMinus, bPlus, bDiv, bMod, bMulti, bEqual;
+    Button btnAC, bEqual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//      hide actionbar
+        // Hide action bar
         getSupportActionBar().hide();
 
+        result = findViewById(R.id.answer);
+        assignBtn(R.id.btn1);
+        assignBtn(R.id.btn2);
+        assignBtn(R.id.btn3);
+        assignBtn(R.id.btn4);
+        assignBtn(R.id.btn5);
+        assignBtn(R.id.btn6);
+        assignBtn(R.id.btn7);
+        assignBtn(R.id.btn8);
+        assignBtn(R.id.btn9);
+        assignBtn(R.id.btn0);
+        assignBtn(R.id.btnMinus);
+        assignBtn(R.id.btnPlus);
+        assignBtn(R.id.btnDiv);
+        assignBtn(R.id.btnMode);
+        assignBtn(R.id.btnMulti);
+        bEqual = findViewById(R.id.btnEqual);
+        btnAC = findViewById(R.id.btnAC);
 
-//        result = findViewById(R.id.answer);
-//        //b1 = findViewById(R.id.btn1); replace in assignBtn() set on listener.
-//        assignBtn(b1, R.id.btn1);
-//        assignBtn(b2, R.id.btn2);
-//        assignBtn(b3, R.id.btn3);
-//        assignBtn(b4, R.id.btn4);
-//        assignBtn(b5, R.id.btn5);
-//        assignBtn(b6, R.id.btn6);
-//        assignBtn(b7, R.id.btn7);
-//        assignBtn(b8, R.id.btn8);
-//        assignBtn(b9, R.id.btn9);
-//        assignBtn(bZero, R.id.btnZero);
-//        assignBtn(bMinus, R.id.btnMinus);
-//        assignBtn(bPlus, R.id.btnPlus);
-//        assignBtn(bDiv, R.id.btnDiv);
-//        assignBtn(bMod, R.id.btnMod);
-//        assignBtn(bMulti, R.id.btnMulti);
-//        assignBtn(bEqual, R.id.btnEqual);
+        // Set click listeners for "Equal" and "AC" buttons
+        bEqual.setOnClickListener(this);
+        btnAC.setOnClickListener(this);
     }
 
-    void assignBtn(Button btn, int id) {
-        btn = findViewById(id);
+    void assignBtn(int id) {
+        Button btn = findViewById(id);
         btn.setOnClickListener(this);
     }
 
@@ -52,17 +56,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String btnText = btn.getText().toString();
         String dataCalculate = result.getText().toString();
 
-        if(btnText.equals("=")) {
-            result.setText("");
-            return;
-        }
-
-        dataCalculate = dataCalculate + btnText;
-        result.setText(dataCalculate);
-
-        String finalResult = getResult(dataCalculate);
-        if (!finalResult.equals("Error")) {
+        if (btnText.equals("AC")) {
+            result.setText("0");
+        } else if (btnText.equals("=")) {
+            String finalResult = getResult(dataCalculate);
             result.setText(finalResult);
+        } else {
+            if (dataCalculate.equals("0")) {
+                dataCalculate = btnText;
+            } else {
+                dataCalculate = dataCalculate + btnText;
+            }
+            result.setText(dataCalculate);
         }
     }
 
@@ -71,11 +76,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Context context = Context.enter();
             context.setOptimizationLevel(-1);
             Scriptable scriptable = context.initStandardObjects();
-            String finalResult =  context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
-            if (finalResult.endsWith(".0")) {
-                finalResult = finalResult.replace(".0", "");
+            Object evalResult = context.evaluateString(scriptable, data, "Javascript", 1, null);
+
+            if (evalResult instanceof Double) {
+                double result = (Double) evalResult;
+                // Check if the result is a whole number (integer)
+                if (result == Math.floor(result)) {
+                    // If it's an integer, convert it to an integer and return as a string
+                    return String.valueOf((int) result);
+                } else {
+                    // If it's a decimal, return it as a string
+                    return String.valueOf(result);
+                }
+            } else {
+                return "Error";
             }
-            return finalResult;
         } catch (Exception e) {
             return "Error";
         }
